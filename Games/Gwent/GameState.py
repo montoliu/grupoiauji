0633d9de77f4b8cc1df8e2df466e2b18769f8d0f
PKTxPepe <al385746@uji.es>
2021-10-25 16:14:04 +0200

    GameState Inicial
    Atributos principales del juego de Gwent

--- Games/Gwent/GameState.py
+++ Games/Gwent/GameState.py
@@ -2,4 +2,18 @@ from Core.GameState import GameState
 
 
 class GwentGameState(GameState):
-    pass
+
+    def __init__(self, lives_p1, lives_p2, points_p1, points_p2, hand_p1, hand_p2, terrain_p1, terrain_p2, turn):
+        self.lives_p1 = lives_p1
+        self.lives_p2 = lives_p2
+        self.points_p1 = points_p1
+        self.points_p2 = points_p2
+        self.hand_p1 = hand_p1
+        self.hand_p2 = hand_p2
+        self.terrain_p1 = terrain_p1
+        self.terrain_p2 = terrain_p2
+        self.turn = turn
+
+    def __str__(self):
+        cadena = str(self.lives_p1) + ", " + self.lives_p2 + ", " + self.points_p1 ", " + self.points_p2 \
+                 + ", " + hand_p1 + ", " + hand_p2 + ", " + terrain_p1 + ", " + terrain_p2 + ", " + turn
