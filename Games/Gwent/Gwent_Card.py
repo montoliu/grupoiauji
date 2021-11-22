c2e4823d0698561b2c99433af8a30ed00537400c
miguelmm95 <79021700+miguelmm95@users.noreply.github.com>
2021-11-22 13:29:58 +0100

    rename card class

--- Games/Gwent/Gwent_Card.py
+++ Games/Gwent/Gwent_Card.py
@@ -1,20 +1,20 @@
-class Card:
+class Gwent_Card:
     def __init__(self, card_name, level, effect):
         self.card_name = card_name
         self.level = level
         self.effect = effect
 
-class MeleeCard(Card):
+class Gwent_Melee_Card(Gwent_Card):
     def __init__(self, card_name, level, effect, card_type = "Melee"):
         super().__init__(card_name, level, effect)
         self.card_type = card_type
 
-class RangedCard(Card):
+class Gwent_Ranged_Card(Gwent_Card):
     def __init__(self, card_name, level, effect, card_type = "Ranged"):
         super().__init__(card_name, level, effect)
         self.card_type = card_type
 
-class ArtilleryCard(Card):
+class Gwent_Artillery_Card(Gwent_Card):
     def __init__(self, card_name, level, effect, card_type = "Artillery"):
         super().__init__(card_name, level, effect)
         self.card_type = card_type
