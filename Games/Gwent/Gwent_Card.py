41e96ea811d9df87fa05e5033e99defcee941510
miguelmm95 <79021700+miguelmm95@users.noreply.github.com>
2021-11-28 20:03:26 +0100

    Gwent_Card functions

--- Games/Gwent/Gwent_Card.py
+++ Games/Gwent/Gwent_Card.py
@@ -4,6 +4,25 @@ class Gwent_Card:
         self.level = level
         self.effect = effect
 
+    def get_type(self):
+        return self.card_type
+
+    def get_level(self):
+        return self.level
+
+    def get_effect(self):
+        return self.effect
+
+    def clone(self):
+        new_card = Gwent_Card(self.card_name,self.level,self.effect,self.card_type)
+        return new_card
+
+    def __str__(self):
+        return self.card_type + str(self.level)
+
+    def __eq__(self,other):
+        return self.card_type == other.card_type and self.level == other.level
+
 class Gwent_Melee_Card(Gwent_Card):
     def __init__(self, card_name, level, effect, card_type = "Melee"):
         super().__init__(card_name, level, effect)
