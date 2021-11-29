bfb0c876ab14f263f5283295be84a389f61e9265
montoliu <montoliu@uji.es>
2021-11-29 09:34:27 +0100

    Clases creadas

---
+++ Games/ClashRoyale/ClashRoyaleForwardModel.py
@@ -0,0 +1,18 @@
+from Core.ForwardModel import ForwardModel
+
+
+class ClashRoyaleForwardModel(ForwardModel):
+    def play(self, game_state, action, heuristic):
+        # Deploy la tropa que diga la action
+        # Mover y/o combatir las tropas en el escenario
+        # Actualizar la vida de las torres y unidades
+        # Actualizar el maná
+        pass
+
+    def check_winner(self, game_state):
+        # Se gana si:
+        # Si no se ha acabado el tiempo y la torre central del otro jugador tiene la vida a cero
+        # Si se ha acabado el tiempo, el ganador es:
+        # a) El jugador que ha desturido más torres
+        # b) Si han destruido más, perderá el que tenga la torre con menos vida
+        pass
---
+++ Games/ClashRoyale/ClashRoyaleGame.py
@@ -0,0 +1,8 @@
+from Core.Game import Game
+
+
+class ClashRoyaleGame(Game):
+    def reset(self, game_state, player_id_as_first):
+        # inicialziar la partida
+        # Crea el estado inicial de la partida
+        pass
