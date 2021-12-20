4604a8aded0721ed7d721220f2657820068c33ef
BohanJi <al405281@uji.es>
2021-12-20 13:37:31 +0100

    Clase cartaClashRoyale

--- Games/ClashRoyale/ClashRoyaleCard.py
+++ Games/ClashRoyale/ClashRoyaleCard.py
@@ -1,25 +1,27 @@
-class Card:
-    def __init__(self, card_name, level, cost):
-        self.card_name = card_name
-        self.level = level
-        self.cost = cost
+class ClashRoyaleCard:
+    def __init__(self):
+        self.damage = 50
+        self.hitpoints = 500
+        self.speed = 1
+        self.hit_range = 1
 
-class TroopCard(Card):
-    def __init__(self, card_name, level, cost, damage, hitpoints, hit_speed, speed, hit_range):
-        super().__init__(self, card_name, level, cost)
-        self.damage = damage
-        self.hitpoints = hitpoints
-        self.hit_speed = hit_speed
-        self.speed = speed
-        self.hit_range = hit_range
 
-class SpellCard(Card):
-    def __init__(self, card_name, level, cost, radius):
-        super().__init__(self, card_name, level, cost)
-        self.radius = radius
+#class TroopCard(Card):
+    #def __init__(self, card_name, level, cost, damage, hitpoints, hit_speed, speed, hit_range):
+        #super().__init__(self, card_name)
+        #self.damage = damage
+        #self.hitpoints = hitpoints
+        #self.hit_speed = hit_speed
+        #self.speed = speed
+        #self.hit_range = hit_range
 
-class BuildingCard(Card):
-    def __init__(self, card_name, level, cost, life_time, hit_points):
-        super().__init__(self, card_name, level, cost)
-        self.life_time = life_time
-        self.hit_points = hit_points
+#class SpellCard(Card):
+    #def __init__(self, card_name, level, cost, radius):
+        #super().__init__(self, card_name)
+        #self.radius = radius
+
+#class BuildingCard(Card):
+    #def __init__(self, card_name, level, cost, life_time, hit_points):
+        #super().__init__(self, card_name)
+        #self.life_time = life_time
+        #self.hit_points = hit_points
